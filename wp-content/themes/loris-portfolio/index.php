a7f2792ec30ee05bf40c8be07b94a6108dc77e55
loris <ducamps.loris@gmail.com>
2025-12-09 21:13:10 +0100

    update index for test cache

--- wp-content/themes/loris-portfolio/index.php
+++ wp-content/themes/loris-portfolio/index.php
@@ -1,6 +1,6 @@
 <?php
 get_header();
 
-echo '<main><h1>Bienvenue sur mon Portfolio WordPress</h1></main>';
+echo '<main><h1>Bienvenue sur mon Portfolio WordPress</h1><p>Voici une description</p></main>';
 
 get_footer();

df90d3d975d7025991148cc31902004fff6f9a3c
loris <ducamps.loris@gmail.com>
2025-12-09 18:49:12 +0100

    initialisation portfolio

---
+++ wp-content/themes/loris-portfolio/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Setup du thème
+ */
+function loris_portfolio_theme_setup() {
+	// Support des images mises en avant
+	add_theme_support( 'post-thumbnails' );
+
+	// Support du title tag
+	add_theme_support( 'title-tag' );
+
+	// Support des menus
+	register_nav_menus(
+		[
+			'main-menu' => __( 'Menu principal', 'loris-portfolio' ),
+		]
+	);
+}
+add_action( 'after_setup_theme', 'loris_portfolio_theme_setup' );
+
+/**
+ * Chargement des assets CSS / JS
+ */
+function loris_portfolio_enqueue_assets() {
+	wp_enqueue_style(
+		'loris-portfolio-style',
+		get_stylesheet_uri(),
+		[],
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'loris_portfolio_enqueue_assets' );
---
+++ wp-content/themes/loris-portfolio/index.php
@@ -0,0 +1,6 @@
+<?php
+get_header();
+
+echo '<main><h1>Bienvenue sur le thème Loris Portfolio</h1></main>';
+
+get_footer();

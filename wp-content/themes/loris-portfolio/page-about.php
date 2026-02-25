e5580177a0e724fd14f37db458daa9851a6f371f
loris <ducamps.loris@gmail.com>
2026-02-25 19:41:55 +0100

    update content pages

---
+++ wp-content/themes/loris-portfolio/page-about.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="page-about">
+    <h1><?php the_title(); ?></h1>
+
+    <section class="portfolio-grid">
+        <p>Ici mon about</p>
+    </section>
+</main>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/loris-portfolio/page-articles.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="page-articles">
+    <h1><?php the_title(); ?></h1>
+
+    <section class="portfolio-grid">
+        <p>Ici l'ensemble de mes articles</p>
+    </section>
+</main>
+
+<?php get_footer(); ?>
